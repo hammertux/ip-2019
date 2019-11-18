4fb2b32ce5e66303adc49d50fac19571438d4a5a
andreadidio98 <andreadidio98@gmail.com>
2019-11-18 18:19:54 +0100

    init

--- lab1/lab1/learning_switch.py
+++ lab1/lab1/learning_switch.py
@@ -6,6 +6,8 @@ from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ipv4
+from ryu.lib.packet import arp
+from ryu.lib.packet import ether_types
 
 
 class LearningSwitch13(app_manager.RyuApp):
@@ -30,13 +32,29 @@ class LearningSwitch13(app_manager.RyuApp):
         # entries in the flow-table, this rule allows the packet
         # to be forwarded to the controller.
         # Note that the priority is the lowest, i.e., 0.
-		
+
 	PRIORITY = 0 # priority const value
 	match = parser.OFPMatch() # Installs a flow
-		
-	actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)] # send match pckt to packet in handler without buffering it
-	self.add_flow(datapath, PRIORITY, match, actions) # add a flow entry
 
+	actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+	self.record_flow(datapath, PRIORITY, match, actions)
+
+
+    def record_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
 
     # Handle the packet_in event
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
@@ -45,7 +63,7 @@ class LearningSwitch13(app_manager.RyuApp):
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        src_port = msg.match['in_port']
+
         # Get Datapath ID to identify OpenFlow switches
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
@@ -60,34 +78,75 @@ class LearningSwitch13(app_manager.RyuApp):
         # packet blocker on specified switches to certain
         # destinations.
         # --------------------------------------------------------
-        
-        pckt = packet.Packet(msg.data) #extract pckt from openflow msg
-        frame = pckt.get_protocols(ethernet.ethernet)[0] #extract outermost ethernet frame
-        pckt_data = None
-        src_mac = frame.src
-        dst_mac = frame.dst
-        self.logger.info("packet in %s %s %s %s", dpid, src_mac, dst_mac, src_port) # log pckt info
+        # learn a mac address to avoid FLOOD next time.
 
-        self.mac_to_port[dpid][src_mac] = src_port # add src mac to port mapping
-        
-        #check if dst is mapped in mac_to_port
-        if dst_mac in self.mac_to_port[dpid]:
-		dst_port = self.mac_to_port[dpid][dst_mac]
-            	match = parser.OFPMatch(in_port=src_port, eth_dst=dst_mac)
-            	actions = [parser.OFPActionOutput(dst_port)] # pass destination port no
-		
-            	if msg.buffer_id == ofproto.OFP_NO_BUFFER:
-                	self.add_flow(datapath, 1, match, actions)
-                	pckt_data = msg.data
-            	else:
-                	self.add_flow(datapath, 1, match, actions, msg.buffer_id)
-                	return 
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+	dst = eth.dst
+        src = eth.src
+
+	# seen in https://github.com/osrg/ryu/blob/v4.4/ryu/app/simple_switch_13.py
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+
+	# check if ARP protocol, in this case learn, forward and don't install flow
+	arppkt = pkt.get_protocol(arp.arp)
+	if arppkt:
+		if dst in self.mac_to_port[dpid]:
+            		out_port = self.mac_to_port[dpid][dst]
+        	else:
+            		out_port = ofproto.OFPP_FLOOD
+		actions = [parser.OFPActionOutput(out_port)]
+		data = None
+        	if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            		data = msg.data
+
+        	out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        	datapath.send_msg(out)
+		return
+
+
+        # implement ip-level filter
+	ip = pkt.get_protocols(ipv4.ipv4)
+        if len(ip):
+	    print(ip)
+            ip = ip[0]
+            if ip.dst == '10.0.0.4':
+                print("dropping packet to h4")
+                return
+
+        dst = eth.dst
+        src = eth.src
+
+        self.mac_to_port[dpid][src] = in_port
+
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
         else:
-            	dst_port = ofproto.OFPP_FLOOD
-            	actions = [parser.OFPActionOutput(dst_port)]
-        
-        forward = parser.OFPPacketOut(datapath, msg.buffer_id, src_port, actions, data)
+            out_port = ofproto.OFPP_FLOOD
 
-        datapath.send_msg(forward)
-        
-     
+        actions = [parser.OFPActionOutput(out_port)]
+
+
+	# at this point only the packets that are not directed towards h4 are present, can install flow
+	if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.record_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.record_flow(datapath, 1, match, actions)
+
+
+	# send the packet
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
--- lab1/lab1/switch_monitor.py
+++ lab1/lab1/switch_monitor.py
@@ -15,17 +15,11 @@ class SwitchMonitor13(LearningSwitch13):
 	def __init__(self, *args, **kwargs):
 		super(SwitchMonitor13, self).__init__(*args, **kwargs)
 
-       	# Maintain a table for all the datapaths in the network
 		self.datapaths = {}
 
-        	# Start a new thread for monitoring
-        	# The new thread excutes the function _monitor
 		self.monitor_thread = hub.spawn(self._monitor)
 
-    	# Handle state change event
-    	# Register/Deregister a datapath
-	@set_ev_cls(ofp_event.EventOFPStateChange,
-                [MAIN_DISPATCHER, DEAD_DISPATCHER])
+	@set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
 	def _state_change_handler(self, ev):
 		datapath = ev.datapath
 		state = ev.state
@@ -39,25 +33,20 @@ class SwitchMonitor13(LearningSwitch13):
     
     
     
-    # Request statistics information from all datapaths every 5s
 	def _monitor(self):
 
 		while True:
 			for dp in self.datapaths.values():
 				self._request_stats(dp)
-					hub.sleep(5)
+			hub.sleep(5)
 
-    	# Request statistics information from a datapath
 	def _request_stats(self, datapath):
 		self.logger.debug('send stats request: %016x', datapath.id)
 		ofproto = datapath.ofproto
 		parser = datapath.ofproto_parser
 
-		request = parser.EventOFPFlowStatsRequest(datapath)
+		request = parser.OFPFlowStatsRequest(datapath)
 		datapath.send_msg(request)
-
-		#request = parser.EventOFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
-		#datapath.send_msg(request)
 
 		# Handle flow stats information from the datapaht
 	@set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
@@ -66,19 +55,13 @@ class SwitchMonitor13(LearningSwitch13):
 		body = ev.msg.body
 		dpid = ev.msg.datapath.id
 
-        	# --------------------------------------------------------
-        	# Your code starts here
-        	# Monitor the specified ports on two switches:
-        	# dpid=1 and dpid=2
-        	# --------------------------------------------------------
 		for flow in body:
-			if dpid == 1 || dpid == 2:
-				self.logger.info(dpid + ' -- ' + flow.match["in_port"])
-
-
-
-
-
+			if dpid == 1:
+				if flow.priority != 0 and (flow.match['in_port'] == 1):
+					print(flow.match['in_port'])
+			elif dpid == 2:
+				if flow.priority != 0 and (flow.actions['port']] == 1):
+					print(flow.match['in_port'])
 
 
 
