c8632a7d0128cf6f78e0fadfa8f0118f8d152353
Giulia Frascaria <giulia.frascaria@gmail.com>
2019-11-19 11:22:29 +0100

    switch monitor test

--- lab1/lab1/switch_monitor.py
+++ lab1/lab1/switch_monitor.py
@@ -30,9 +30,7 @@ class SwitchMonitor13(LearningSwitch13):
 		else:
 			if datapath.id == DEAD_DISPATCHER:
 				del self.datapaths[datapath.id]
-    
-    
-    
+
 	def _monitor(self):
 
 		while True:
@@ -55,17 +53,48 @@ class SwitchMonitor13(LearningSwitch13):
 		body = ev.msg.body
 		dpid = ev.msg.datapath.id
 
-		for flow in body:
+		# need to monitor datapath 1 and datapath 2
+		self.logger.info('datapath         '
+                     'in-port  eth-dst           '
+                     'out-port packets')
+
+    		self.logger.info('---------------- '
+                     '-------- ----------------- '
+                     '-------- -------- ')
+		'''for flow in body:
 			if dpid == 1:
 				if flow.priority != 0 and (flow.match['in_port'] == 1):
 					print(flow.match['in_port'])
 			elif dpid == 2:
-				if flow.priority != 0 and (flow.actions['port']] == 1):
-					print(flow.match['in_port'])
+				if flow.priority != 0 and (flow.actions['port'] == 1):
+					print(flow.match['in_port'])'''
 
+		for flow in body:
+			#if dpid == 1:
+				match = flow.match
+				if flow.priority != 0:
+					self.logger.info('%016x %8x %17s %8x %8d',
+                                	ev.msg.datapath.id,
+                                	flow.match['in_port'],
+                                	flow.match['eth_dst'],
+                                	flow.instructions[0].actions[0].port,
+                                	flow.packet_count)
 
+			#elif dpid == 2:
+			#	if flow.priority != 0 and (flow.action['port'] == 1):
+                        #                self.logger.info('%016x %8x %17s %8x %8d',
+                        #                ev.msg.datapath.id,
+                        #                flow.match['in_port'],
+                        #                flow.match['eth_dst'],
+                        #                flow.instructions[0].actions[0].port,
+                        #                flow.packet_count)
 
-
-
-
-
+		'''for stat in sorted([flow for flow in body if flow.priority == 1],
+                       key=lambda flow: (flow.match['in_port'],
+                                         flow.match['eth_dst'])):
+        		self.logger.info('%016x %8x %17s %8x %8d %8d',
+                        	ev.msg.datapath.id,
+                         	stat.match['in_port'],
+				stat.match['eth_dst'],
+                         	stat.instructions[0].actions[0].port,
+                         	stat.packet_count)'''
