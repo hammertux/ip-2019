a9f343a9a0fa75080b5c215900377a6c75e2685f
Giulia Frascaria <giulia.frascaria@gmail.com>
2019-12-12 10:24:15 +0100

    redundancy su 3 switch e receiver in h3 per le statistiche

---
+++ lab3/lab3/reliable/receive.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+from scapy.all import sniff, sendp
+from scapy.all import Packet
+from scapy.all import ShortField, IntField, LongField, BitField
+from scapy.all import RTP
+
+import sys
+import struct
+import pdb
+def handle_pkt(pkt_in):
+    pkt = str(pkt_in)
+    # if len(pkt) < 12: return
+    if pkt_in[0]["UDP"]:
+        pkt_in[0]["UDP"].payload = RTP(pkt_in[0]["Raw"].load)   
+        #pkt_in[0]["RTP"].show()
+        print(pkt_in[0]["RTP"].sourcesync)
+        #sys.stdout.flush()
+
+    # if msg_type < 4:
+    # print pkt_in.show()
+    
+
+def main():
+    sniff(filter="udp and port 5004",
+            iface = "eth0",
+          prn = lambda x: handle_pkt(x))
+
+if __name__ == '__main__':
+    main()
